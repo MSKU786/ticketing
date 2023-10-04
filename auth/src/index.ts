bd0db2374b1e84284d1f2831baab0bad9ab3d971
MSKU786 <manishsingh332000@gmail.com>
2023-10-04 11:52:38 +0530

    Connecting to mongo db

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+
 import 'express-async-errors';
 const app = express();
 app.use(json());
@@ -21,6 +24,16 @@ app.all('*', async () => {
   throw new NotFoundError();
 });
 
+const startDB = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 app.listen(4000, () => {
   console.log('listening on port 4000!!!!!');
 });
+
+startDB();
