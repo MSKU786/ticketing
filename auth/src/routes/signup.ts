ed508d99955a22180cc7ebd3e3cac3b893fd7f38
MSKU786 <manishsingh332000@gmail.com>
2023-10-07 12:55:16 +0530

    Generating a JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -33,6 +34,17 @@ router.post(
     const user = User.build({ email, password });
     await user.save();
 
+    //Generate Web token
+    const jwtToken = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      'asdf'
+    );
+
+    //Store it on the session object
+    req.session = { jwt: jwtToken };
     res.status(201).send(user);
   }
 );
