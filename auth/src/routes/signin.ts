02ca2c75ff77c3d11017656c17d8157d59e86c4a
MSKU786 <manishsingh332000@gmail.com>
2023-10-08 14:30:11 +0530

    Added sign in flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,22 @@
-import express, { Router } from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/user/signin', (req, res) => {
-  res.send('hi there ~~~~ :) we are signin');
-});
+router.post(
+  '/api/user/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Password is not correct'),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+  }
+);
 
 export { router as signinRouter };
