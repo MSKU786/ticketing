75898dc2efc9f1b9f9d869838b31f6bf4c5a441b
MSKU786 <manishsingh332000@gmail.com>
2023-10-09 07:33:42 +0530

    Current User Handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,18 @@
 import express, { Router } from 'express';
-
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 router.get('/api/user/currentUser', (req, res) => {
-  res.send('hi there ~~~~ :)');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.send({ currentUser: payload });
+  } catch (err) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,15 +1,49 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 router.post(
-  '/api/user/signin',
+  '/api/existingUser/signin',
   [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().notEmpty().withMessage('Password is not correct'),
   ],
   validationResult,
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('INvalid Credentials');
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError('INvalid Credentials');
+    }
+
+    const jwtToken = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      //To override tS functionality to check because we alredy define this key
+      process.env.JWT_KEY!
+    );
+
+    //Store it on the session object
+    req.session = { jwt: jwtToken };
+    res.status(201).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
