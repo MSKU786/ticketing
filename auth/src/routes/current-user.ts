18677053fda5b56a63da812070dc964339512ae8
MSKU786 <manishsingh332000@gmail.com>
2023-10-09 07:58:50 +0530

    requiring auth for route access

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express';
 import jwt from 'jsonwebtoken';
 import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 const router = express.Router();
 
-router.get('/api/user/currentUser', currentUser, (req, res) => {
+router.get('/api/user/currentUser', currentUser, requireAuth, (req, res) => {
   return res.send({ currentUser: req.currentUser || null });
 });
 
