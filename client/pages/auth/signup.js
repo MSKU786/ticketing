75e8c9d2c5ff4c42855282b00643bf7e915932e1
MSKU786 <manishsingh332000@gmail.com>
2023-10-14 21:44:19 +0530

    Successfull account singup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,15 +1,29 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const submit = async (event) => {
+    event.preventDefault();
+    console.log(email, password);
+
+    const res = await axios.post('/api/user/signup', {
+      email,
+      password,
+    });
+
+    console.log(res.data);
+  };
+
   return (
-    <form>
+    <form onSubmit={submit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
         <input
           value={email}
-          onChange={(e) => setEmail(e.value)}
+          onChange={(e) => setEmail(e.target.value)}
           className="form-control"
         />
       </div>
@@ -17,11 +31,12 @@ export default () => {
         <label>Password</label>
         <input
           value={password}
+          onChange={(e) => setPassword(e.target.value)}
           type="password"
           className="form-control"
         />
       </div>
-      <button className="btn btn-primar">Sign Up</button>
+      <button className="btn btn-primary">Sign Up</button>
     </form>
   );
 };
