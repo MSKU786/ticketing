676118228171916112900c72486f4435651b3a6e
MSKU786 <manishsingh332000@gmail.com>
2023-10-19 09:31:29 +0530

    Moving get initial props on app component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
+
+AppComponent.getInitialProps = async (context) => {};
+
+export default AppComponent;
