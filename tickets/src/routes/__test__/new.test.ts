07d686306823f7cb1b0b3a406ca5fa3ff70bda3d
MSKU786 <manishsingh332000@gmail.com>
2023-10-23 21:55:13 +0530

    Added ticket deployemnt  fie's

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,8 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('has a route handler listening to /api/tickets for post request', async () => {});
+it('can only be accessed iuf user signed in', async () => {});
+it('return an error if an invalid title is provided', async () => {});
+it('return an error if an invalid price is provided', async () => {});
+it('creates a ticket with valid input', async () => {});
