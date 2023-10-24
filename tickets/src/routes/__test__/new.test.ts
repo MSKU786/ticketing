02fb562d1951e8fdb56ae90ed639e68e1d613f9a
MSKU786 <manishsingh332000@gmail.com>
2023-10-24 21:55:59 +0530

    Added new testcase with new global.signin function

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -11,6 +11,14 @@ it('can only be accessed iuf user signed in', async () => {
   await request(app).post('/api/tickets').send({}).expect(401);
 });
 
+it('Not return 401 if user signed in', async () => {
+  const res = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({});
+  expect(res.status).not.toEqual(401);
+});
+
 it('return an error if an invalid title is provided', async () => {});
 it('return an error if an invalid price is provided', async () => {});
 it('creates a ticket with valid input', async () => {});
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,11 +2,12 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
 
 let mongo: any;
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 beforeAll(async () => {
@@ -32,18 +33,26 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
+global.signin = () => {
+  const payload = {
+    id: 'ajlk2341',
+    email: 'test@test.com',
+  };
 
-  const res = await request(app)
-    .post('/api/user/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201);
+  // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  const cookie = res.get('Set-Cookie');
-  return cookie;
+  // Build session object {jwt: 'MY_JWT'}
+  const session = {
+    jwt: token,
+  };
+
+  // Turn the sesion into json
+  const sessionJSON = JSON.stringify(session);
+
+  //Take json and encode it as base64
+
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  return [`session=${base64}`];
 };
