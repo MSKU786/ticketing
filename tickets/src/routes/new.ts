85627a7df1ded7b7edb237a61a540f6a7a91fb49
MSKU786 <manishsingh332000@gmail.com>
2023-10-26 14:42:41 +0530

    Creation of ticket route handler

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { requireAuth, validateRequest } from '@ticcketing/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -14,8 +15,18 @@ router.post(
       .withMessage('Price must be greater than zero'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    return res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+
+    await ticket.save();
+
+    return res.status(201).send(ticket);
   }
 );
 
