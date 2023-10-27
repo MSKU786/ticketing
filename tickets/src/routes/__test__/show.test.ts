c9cdb028128ac6e7c80f3f2f9a106c7d3b455072
MSKU786 <manishsingh332000@gmail.com>
2023-10-27 10:02:58 +0530

    Added id search test and bug fix

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,12 +1,22 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import mongoose from 'mongoose';
 
 it('return a 404 if the ticket .is not found', async () => {
-  const res = await request(app)
-    .get('/api/tickets/falsdkjfkajad')
-    .send()
-    .expect(404);
+  const id = new mongoose.Types.ObjectId().toHexString();
+  const res = await request(app).get(`/api/tickets/${id}`).send().expect(404);
+});
 
-  console.log(res.body);
+it('returns 200 if the ticket search is successfull', async () => {
+  const ticket = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'hey',
+      price: '123',
+    })
+    .expect(201);
+
+  await request(app).get(`/api/tickets/${ticket.body.id}`).send().expect(200);
 });
--- tickets/src/routes/show.ts
+++ tickets/src/routes/show.ts
@@ -9,17 +9,14 @@ import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.get(
-  '/api/tickets/:id',
-  requireAuth,
-  async (req: Request, res: Response) => {
-    const ticketId = req.params.id;
-    const ticket = Ticket.findById(ticketId);
-    if (!ticket) {
-      throw new NotFoundError();
-    }
-    return res.status(200).send(ticket);
-  }
-);
+router.get('/api/tickets/:id', async (req: Request, res: Response) => {
+  const ticketId = req.params.id;
 
-export { router as createTicketRouter };
+  const ticket = await Ticket.findById(ticketId);
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+  return res.status(200).send(ticket);
+});
+
+export { router as showTicketRouter };
