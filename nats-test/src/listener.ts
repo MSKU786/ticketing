6a95fe8baf7aac538b9d4afe1c04261b06db511e
MSKU786 <manishsingh332000@gmail.com>
2023-11-03 06:28:38 +0530

    Added queue group to avoid listen to multople events

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,25 @@
-import nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+import nats, { Message } from 'node-nats-streaming';
 console.clear();
 
 // it's same as client
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'https://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'order-service-queue-group'
+  );
 
-  subscription.on('message', (msg) => {
-    console.log('Listening to event');
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data == 'string') {
+      console.log(`Recieved event ${msg.getSequence()}, with data ${data}`);
+    }
   });
 });
