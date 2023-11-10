0f526ec7b6906a5bece7529a30a0dac0f865ec2a
MSKU786 <manishsingh332000@gmail.com>
2023-11-10 11:24:34 +0530

    Singleton Implementation for ticketing service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const startDB = async () => {
   if (!process.env.JWT_KEY) {
@@ -10,6 +11,7 @@ const startDB = async () => {
     throw new Error('Mongo URL must be define');
   }
   try {
+    await natsWrapper.connect('ticketing', 'awerjl', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI);
   } catch (err) {
     console.error(err);
