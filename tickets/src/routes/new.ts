8e31b321dac4cba138ac663aa6f18f23649ae56b
MSKU786 <manishsingh332000@gmail.com>
2023-11-15 09:44:54 +0530

    Accessing the NATS client

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,8 @@ import { requireAuth, validateRequest } from '@ticcketing/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
+import { Stan } from 'node-nats-streaming';
 
 const router = express.Router();
 
@@ -26,7 +28,7 @@ router.post(
     });
 
     await ticket.save();
-    await new TicketCreatedPublisher(client).publish({
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
