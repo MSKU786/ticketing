0649fde981f402344745bbaed6437f8b039466cf
MSKU786 <manishsingh332000@gmail.com>
2023-11-28 10:16:48 +0530

    Subtle service coupling

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from 'express';
-
+import { requireAuth, validateRequest } from '@ticcketing/common';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  return res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('Ticket id must be porvided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    return res.send({});
+  }
+);
 
 export { router as newOrderRouter };
