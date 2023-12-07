82d0206efd2eb4811e3df3b3b0a2f01c79842e14
MSKU786 <manishsingh332000@gmail.com>
2023-12-07 10:03:32 +0530

    Simple on message implementation

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,9 +1,16 @@
 import { Listener, Subjects, TicketCreatedEvent } from '@ticcketing/common';
 import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName: string = 'order-service';
+  queueGroupName: string = queueGroupName;
 
-  onMessge(data: TicketCreatedEvent['data'], msg: Message): void {}
+  async onMessge(data: TicketCreatedEvent['data'], msg: Message) {
+    const { title, price } = data;
+    const ticket = Ticket.build({ title, price });
+    await ticket.save();
+    msg.ack();
+  }
 }
