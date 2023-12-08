d8a7a7c7ac32fd84d60dbb80934db3d0ed87396c
MSKU786 <manishsingh332000@gmail.com>
2023-12-08 09:48:32 +0530

    initalized listeners

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const startDB = async () => {
   if (!process.env.JWT_KEY) {
@@ -37,6 +39,12 @@ const startDB = async () => {
 
     //on process termaination
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    //intializing the ticketlisteners
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI);
   } catch (err) {
     console.error(err);
