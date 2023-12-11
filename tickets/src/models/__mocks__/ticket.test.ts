58b39b574e3399fd00c4e94bb50f7c3cf01fff68
MSKU786 <manishsingh332000@gmail.com>
2023-12-11 14:36:07 +0530

    Added test case for occ

--- tickets/src/models/__mocks__/ticket.test.ts
+++ tickets/src/models/__mocks__/ticket.test.ts
@@ -1,6 +1,34 @@
+import { Ticket } from '../ticket';
+
 it('implemetns optimistic currency control', async () => {
+  //Create am omstamce of ticket
+
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123',
+  });
+
+  //Save the ticket to db
+  await ticket.save();
+
+  //Fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  //Two seprate changes to the instance we fetched
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 20 });
+
+  //save the first fetched ticket
+  await firstInstance!.save();
+
+  //save the second ticket and expect an error
   try {
+    await secondInstance!.save();
   } catch (err) {
     return;
   }
+
+  throw new Error('should not reach here');
 });
