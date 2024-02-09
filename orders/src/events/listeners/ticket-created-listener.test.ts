a620183b406eb04a7d75b743a2db0a3b5efddc18
MSKU786 <manishsingh332000@gmail.com>
2024-02-09 12:00:34 +0530

    Testing listener procedure completed

---
+++ orders/src/events/listeners/ticket-created-listener.test.ts
@@ -0,0 +1,15 @@
+const setup = async () => {
+  // creates an instance of listener
+  // creates a fake data event
+  // creates a fake message object
+};
+
+it('creates and saves a ticket', async () => {
+  // calls the onMessage function with the data object + message object
+  // write assertion to make sure a data is created
+});
+
+it('acks the message', async () => {
+  // calls the onMessage function with the data object + message object
+  // write a function to make sure assr is called
+});
