20e26ebabe42ff0f04fa920f845b78546fb1ae02
MSKU786 <manishsingh332000@gmail.com>
2024-06-26 10:04:04 +0530

    Making order cancelled listener

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,29 @@
+import {
+  Listener,
+  OrderCancelledEvent,
+  OrderCreatedEvent,
+  OrderStatus,
+  Subjects,
+} from '@ticcketing/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+  queueGroupName: string = queueGroupName;
+  async onMessge(data: OrderCreatedEvent['data'], msg: Message): void {
+    const order = await Order.findOne({
+      _id: data.id,
+      version: data.version - 1,
+    });
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+    msg.ack();
+  }
+}
