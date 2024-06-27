1165bac38a4d21a7b892232ce4ce8d3ab8d9abaf
MSKU786 <manishsingh332000@gmail.com>
2024-06-27 10:02:06 +0530

    validating order payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,15 @@
-import { requireAuth, validateRequest } from '@ticcketing/common';
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  currentUser,
+  requireAuth,
+  validateRequest,
+} from '@ticcketing/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { Order } from '../models/order';
 const router = express.Router();
 
 router.post(
@@ -9,6 +18,22 @@ router.post(
   [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { orderId, token } = req.body();
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for cancelled order');
+    }
+
     res.send({ success: true });
   }
 );
