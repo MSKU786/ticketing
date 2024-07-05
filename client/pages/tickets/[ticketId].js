0f864f25cdc8ea04a58e012ce43dbe59626cb20f
MSKU786 <manishsingh332000@gmail.com>
2024-07-05 10:24:45 +0530

    Adding a order

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,5 +1,36 @@
-const TicketShow = () => {
-  return <div>Ticket Show</div>;
+import useRequest from '../../hooks/use-request';
+
+const TicketShow = ({ ticket }) => {
+  const { doRequest, errros } = useRequest({
+    url: '/api/orders',
+    post: 'post',
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: (order) => console.log(order),
+  });
+
+  return (
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+      {errros}
+      <button
+        onClick={doRequest()}
+        className="btn btn-primary"
+      >
+        {' '}
+        Purchase{' '}
+      </button>
+    </div>
+  );
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+  return { ticket: data };
 };
 
 export default TicketShow;
